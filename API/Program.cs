9ee24f31b31137ee8eb93903b6d55b863443120a
motzuo <marecsko0502@hotmail.com>
2022-11-02 16:05:35 +0100

    just a try

--- API/Program.cs
+++ API/Program.cs
@@ -32,7 +32,7 @@ namespace API
             catch(Exception ex)
             {
                 var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex, "An error occured during migration");
+                logger.LogError(ex, "An error occured during migration!");
             }
 
             await host.RunAsync();
